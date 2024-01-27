8268b72977c9dc55d0c27c91ccb79514de96f78e
prijais1102 <priyajaiswal.mzp1@gmail.com>
2024-01-28 04:06:28 +0530

    Updates

--- CSharp5/Program.cs
+++ CSharp5/Program.cs
@@ -2,6 +2,7 @@
 using StringFunctions;
 using static System.Net.Mime.MediaTypeNames;
 using System.Text;
+using System;
 
 namespace ExtensionMethodsDemo
 {
@@ -19,8 +20,11 @@ namespace ExtensionMethodsDemo
                 Console.WriteLine("Enter a string to check for palindrome");
                 string s = Console.ReadLine();
                 Console.WriteLine(stringFunctions.Palindrome(s));
-                Program program = new Program();
-                Console.WriteLine(numericFunctions.Table(10));
+                //Console.WriteLine(numericFunctions.Table());
+                ////Console.WriteLine(numericFunctions.EvenNos(1, 8));
+                //Console.WriteLine(numericFunctions.FindPrimeNos());
+                //Console.WriteLine(numericFunctions.TableOneToTen());
+                Console.WriteLine(numericFunctions.ReverseNumber());
             }
             catch (Exception ex)
             {
@@ -31,15 +35,127 @@ namespace ExtensionMethodsDemo
 
     static class ExtendedProgram
     {
-        public static string Table(this NumericFunctions.NumericFunctions nm, int num)
+        public static string EvenNos(this NumericFunctions.NumericFunctions nm, int startnumber, int endnumber)
+        {
+            Console.WriteLine("Even numbers in this range-");
+            for (int i = startnumber; i <= endnumber; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            return "Even nos are displayed";
+
+        }
+
+        public static string OddNos(this NumericFunctions.NumericFunctions nm, int startnumber, int endnumber)
+        {
+            Console.WriteLine("Odd numbers in this range");
+            for (int i = startnumber; i <= endnumber; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            return "Odd nos are displayed";
+        }
+
+        public static string FindPrimeNos(this NumericFunctions.NumericFunctions nm)
+        {
+            Console.WriteLine("Enter start and end nos.");
+            int startno = int.Parse(Console.ReadLine());
+            int endno = int.Parse(Console.ReadLine());
+            int count = 0;
+            Console.WriteLine("Prime nos within this range-");
+            for (int i = startno; i <= endno; i++)
+            {
+                int prime = 0;
+                if (i > 1)
+                {
+                    for (int j = 2; j < i; j++)
+                    {
+                        if (i % j == 0)
+                        {
+                            prime = 1;
+                            break;
+                        }
+                    }
+                    if (prime == 0)
+                    {
+                        Console.WriteLine(i);
+                        count++;
+                    }
+                }
+
+            }
+            return "Prime nos are displayed and their count is "+count;
+        }
+
+
+        public static string Table(this NumericFunctions.NumericFunctions nm)
         {
             StringBuilder sb = new StringBuilder();
+            Console.WriteLine("Enter no. to get it's table");
+            int num=int.Parse(Console.ReadLine());
             for (int i = 1; i <= 10; i++)
             {
                 sb.AppendLine($"{num} * {i} = {num * i}");
             }
             return sb.ToString();
         }
+
+        public static string TableOneToTen(this NumericFunctions.NumericFunctions nm)
+        {
+            StringBuilder sb = new StringBuilder();
+            Console.WriteLine("Enter range of nos.");
+            int start=int.Parse(Console.ReadLine());
+            int end=int.Parse(Console.ReadLine());
+            for (int j = start; j <= end; j++)
+            {
+                for (int i = 1; i <= 10; i++)
+                {
+                    sb.AppendLine($"{j} * {i} = {j * i}");
+                }
+            }
+            return sb.ToString();
+        }
+        public static string TableRange(this NumericFunctions.NumericFunctions nm)
+        {
+            StringBuilder sb = new StringBuilder();
+            Console.WriteLine("Enter range of nos.");
+            int start = int.Parse(Console.ReadLine());
+            int end = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter range of tables");
+            int range1=int.Parse(Console.ReadLine());
+            int range2=int.Parse(Console.ReadLine());
+            for (int j = start; j <= end; j++)
+            {
+                for (int i = range1; i <= range2; i++)
+                {
+                    sb.AppendLine($"{j} * {i} = {j * i}");
+                }
+                
+            }
+            return sb.ToString();
+        }
+
+        public static int ReverseNumber(this NumericFunctions.NumericFunctions nm)
+        {
+            Console.WriteLine("Enter a no");
+            int num=int.Parse(Console.ReadLine());
+            int sum = 0;
+            while (num > 0)
+            {
+                int d = num % 10;
+                sum = (sum*10) +d;
+                num /= 10;
+
+            }
+            return sum;
+        }
+
     }
 }
 
--- StringFunctions/StringFunctions.cs
+++ StringFunctions/StringFunctions.cs
@@ -1,23 +1,175 @@
-﻿namespace StringFunctions
+﻿using System.Globalization;
+using System.Text.RegularExpressions;
+
+namespace StringFunctions
 {
     public class StringFunctions
     {
+        public int NoOfCharInSentence(string sentence)
+        {
+
+            return sentence.Length;
+
+        }
         public bool Palindrome(string s)
         {
             char[] stringArray = s.ToCharArray();
 
-            string reverse = "";
-
-            for (int i = stringArray.Length - 1; i >= 0; i--)
-            {
-                reverse += stringArray[i];
-            }
-            if(reverse.Equals(s))
+            Array.Reverse(stringArray);
+            string str=new string(stringArray);
+            
+            if(str.ToString().Equals(s))
             {
                 return true;
             }
             return false;
         }
+        public string ReverseSentence(string sentence)
+        {
+            string result = "";
+
+                for (int i = sentence.Length - 1; i >= 0; i--)
+                {
+                    result += sentence[i];
+                }
+          
+          
+
+
+            return result;
+
+        }
+        public void FindAllTypesChar(string sentence)
+        {
+
+
+            int consonants = 0;
+            int vowels = 0;
+            int integers = 0;
+            int special = 0;
+            
+                for (int i = 0; i < sentence.Length; i++)
+                {
+                    if (sentence[i] >= 'A' && sentence[i] <= 'Z' || sentence[i] >= 'a' && sentence[i] <= 'z')
+                    {
+                        if (sentence[i] == 'A' || sentence[i] == 'a' || sentence[i] == 'E' || sentence[i] == 'e'
+                            || sentence[i] == 'I' || sentence[i] == 'i' || sentence[i] == 'O' || sentence[i] == 'o' ||
+                            sentence[i] == 'U' || sentence[i] == 'u')
+                        {
+                            vowels++;
+                        }
+                        else
+                        {
+                            consonants++;
+                        }
+
+                    }
+                    if (sentence[i] >= '0' && sentence[i] <= '9')
+                    {
+                        integers++;
+                    }
+                    if (sentence[i] == '@' || sentence[i] == ' ' || sentence[i] == '#' || sentence[i] == '$')
+                    {
+                        special++;
+                    }
+
+                }
+                Console.WriteLine("No of integers : " + integers);
+                Console.WriteLine("No of Vowels : " + vowels);
+                Console.WriteLine("No of Consonants : " + consonants);
+                Console.WriteLine("No of Special Characters : " + special);
+            }
+        public string StringToUpper(string sentence)
+        {
+            return sentence.ToUpper();
+
+        }
+        public string StringToProper(string sentence)
+        {
+            var textinfo = CultureInfo.CurrentCulture.TextInfo;
+            return textinfo.ToTitleCase(sentence);
+
+        }
+        public string CombineTwoSentences(string sentence1, string sentence2)
+        {
+
+
+            return sentence1 = sentence1 + " " + sentence2;
+
+        }
+        public string RemoveExtraSpaces(string sentence)
+        {
+            return Regex.Replace(sentence, @"\s+", " ").Trim();
+        }
+        public int CountNoOfWords(string sentence)
+        {
+           
+            int count = 0;
+           
+                for (int i = 0; i < sentence.Length; i++)
+                {
+                    if (sentence[i].Equals(" "))
+                    {
+                        count++;
+                    }
+
+                }
+            return count;
+        }
+        public bool FindSubstring(string sentence, string substring)
+        {
+            bool temp;
+           
+                if (sentence.Contains(substring))
+                {
+                    temp = true;
+
+                }
+                else
+                {
+                    temp = false;
+                }
+            
+
+            return temp;
+        }
+        public int NoOfOccurrenceOfSubString(string sentence, string substring)
+        {
+            int count = 0;
+                for (int i = 0; i < sentence.Length - substring.Length + 1; i++)
+                {
+                    if (sentence.Substring(i, substring.Length) == substring)
+                    {
+                        count++;
+                    }
+                }
+            
+            return count;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     }
-}
+
+
+    }
+
